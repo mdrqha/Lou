1ca9d2e7dad69ab5171bd37153e2641fc1129bc3
mdrqha <mdrqha@gmail.com>
2024-08-10 19:51:02 +0200

    add feathure icon, add component menu buttons

---
+++ lou-react/src/Components/Menu-button/Menu-button.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Menu-button.scss"
+
+// Composant de bouton de menu généralisé
+const MenuButton = ({ text, onClick, type = 'button', className = '', icon, isActive = false }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`lou-flex lou-gap-xs lou-w-full lou-p-xs lou-items-center lou-rounded-sm hover:lou-bg-dark-50 lou-transition-all lou-duration-300 ${isActive ? 'menu--item-active' : ''} ${className}`}
+    >
+      {icon && <span className="lou-opacity-400">{icon}</span>} {/* Affiche l'icône s'il est passé en prop */}
+      <p>{text}</p>
+    </button>
+  );
+};
+
+export default MenuButton;
