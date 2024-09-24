23df0bea60f7b31ca674cfa960cc730f92082999
frontcodelover <contact@avantjetaisriche.com>
2024-09-24 15:59:48 +0200

    FEAT: collapse menu v1 done

--- frontend/src/Components/Buttons/Menu-button/Menu-button.jsx
+++ frontend/src/Components/Buttons/Menu-button/Menu-button.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import "./Menu-button.scss";
-
+import './Menu-button.scss';
 
 const MenuButton = ({ text, onClick, type = 'button', className = '', icon, isActive = false, isMenuClosed }) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className={`lou-flex ${isMenuClosed? 'lou-justify-center' : 'lou-w-full lou-gap-xs'} lou-p-xs lou-items-center lou-rounded-sm hover:lou-bg-dark-50 lou-transition-all lou-duration-300 ${isActive ? 'menu--item-active' : ''} ${className}`}
+      className={`lou-flex ${
+        isMenuClosed ? 'lou-justify-center ' : 'lou-w-full lou-gap-xs'
+      } lou-p-xs lou-my-1 lou-items-center lou-rounded-sm hover:lou-bg-dark-50 lou-transition-all lou-duration-300 ${isActive ? 'menu--item-active' : ''} ${className}`}
     >
       {icon && <span className='lou-opacity-400'>{icon}</span>} {/* Affiche l'icône s'il est passé en prop */}
-      <p>{isMenuClosed ? '' : text}</p>
+      <p className='lou-line-clamp-1'>{isMenuClosed ? '' : text}</p>
     </button>
   );
 };
